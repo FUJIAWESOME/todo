23a7db8b99cb041d34b055b7d537ca07e75239e5
FUJIAWESOME <dkorotchikov123@gmail.com>
2023-03-14 10:47:04 +0300

    added func showList

--- todo.js
+++ todo.js
@@ -16,12 +16,30 @@ function deleteTask(task) {
     delete this[task];
 }
 
+function showList() {
+    console.log("ToDo:");
+    for (let key in this) {
+        if (this[key] === "To Do") console.log(` "${key}",`)
+    }
+    console.log("In Progress:");
+    for (let key in this) {
+        if (this[key] === "In Progress") console.log(` "${key}",`)
+    }
+    console.log("Done:");
+    for (let key in this) {
+        if (this[key] === "Done") console.log(` "${key}",`)
+    }
+}
+
 list.changeStatus = changeStatus;
 list.addTask = addTask;
 list.deleteTask = deleteTask;
+list.showList = showList;
 
 console.log(list);
 list.changeStatus("write a post", "Done");
 list.addTask("write a todo list");
 list.deleteTask("make a bed");
+list.showList();
 console.log(list);
+
